eaa6be7eccd4ec034bce865bb2ef04f8c6ce826a
Shavem <shivam.aarya2017@gmail.com>
2022-01-13 14:00:50 -0600

    Did a lot of things

--- loginForm.py
+++ loginForm.py
@@ -1,18 +1,51 @@
 from tkinter import *
 from tkinter.ttk import Style
 from tkinter import messagebox
-import csv
-
+import pandas as pd
+from datetime import datetime
+import time
 
 # TODO: Make it auto-upload to github when closed
+
 # File import
-# f = open("RoboticsHourLog.csv", "w")
-# csv_reader = csv.reader(f, delimiter=",")
-# headers = []
-# for row in csv_reader:
-#     headers.append(row)
-#     break
-# print(headers)
+df = pd.read_csv("RoboticsHourLog.csv")
+df.to_csv("RoboticsHourLog.csv", index=False)
+
+# Create cache to store all people that have signed in and not signed out
+signed_in = []
+
+# Reset today's date and create column if needed
+now = datetime.now()
+current_date = now.strftime("%Y-%m-%d")
+if not current_date in df.columns:
+    df[current_date] = ""
+
+
+# Time manipulation functions
+def time_difference(start_time, end_time):
+    og = start_time.split(":")
+    new = end_time.split(":")
+    return (int((new[0])) - int(og[0])) * 3600 + (int(new[1]) - int(og[1])) * 60 + (int(new[2]) - int(og[2]))
+
+def seconds_to_time(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = (seconds % 3600) % 60
+    return str(hours) + ":" + str(minutes) + ":" + str(seconds)
+
+def time_to_seconds(time):
+    hours = int(time.split(":")[0])
+    minutes = int(time.split(":")[1])
+    seconds = int(time.split(":")[2])
+    return hours * 3600 + minutes * 60 + seconds
+
+# Converts a list into a string
+def list_to_string(list):
+    string = ""
+    for i in list:
+        string += i + ", "
+    return string[:-2]
+
 
 # root window
 
@@ -21,14 +54,25 @@ root = Tk()
 root.title("Robotics Sign-In Form")
 
 # Closing
-def on_closing():
-    # TODO: decide default hours for someone who doesn't sign out and list them all in this messagebox and ask this messagebox only if there are people that haven't signed out
-    if messagebox.askokcancel("Quit", "Do you want to quit? The following people have not signed out and will be set to 'Not Signed Out' with a default of _ hours:"):
-        root.destroy()
-        # TODO: set default hours for people who didn't sign out and add the "not signed out" attribute
-        print("Set everyone to _ hours by default and add 'did not sign out' to it")
+def on_closing(df):
+    # TODO: open new window allowing user to input number of hours for people that haven't signed out
+    if len(signed_in) > 0:
+        if messagebox.askokcancel("Quit", f"Do you want to quit? The following people have not signed out and will be set to a default of 1 hour: {list_to_string(signed_in)}"):
+            # TODO: set default hours for people who didn't sign out and add the "not signed out" attribute
+            print("Set everyone to _ hours by default and add 'did not sign out' to it")
+            save_df = df.sort_values(by=["Hours"], ascending=False)
+            save_df.to_csv("RoboticsHourLog.csv", index=False)
+            print(save_df)
+            root.destroy()
+    else:
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            save_df = df.sort_values(by=["Hours"], ascending=False)
+            save_df.to_csv("RoboticsHourLog.csv", index=False)
+            print(save_df)
+            root.destroy()
 
-root.protocol("WM_DELETE_WINDOW", on_closing)
+
+root.protocol("WM_DELETE_WINDOW", lambda: on_closing(df))
 
 
 # Instructions
@@ -37,28 +81,54 @@ myLabel = Label(root, text="Find for your name:")
 
 
 # Pick name
-# TODO: Make the names ordered by most hours, add keybinds tot he listbox
+# TODO: Add keybinds to the listbox
 name_frame = Frame(root)
 # Scrollbar
 name_scrollbar = Scrollbar(name_frame, orient=VERTICAL)
-name_box = Listbox(name_frame, yscrollcommand=name_scrollbar.set)
+name_box = Listbox(name_frame, yscrollcommand=name_scrollbar.set, width=30)
 name_scrollbar.config(command=name_box.yview)
-with open("members.txt", "r") as f:
-    for line in f:
-        name_box.insert(END, line.strip())
+
+
+names = []
+# Populate listbox
+for row in df.iterrows():
+    name_box.insert(END, row[1]["Name"])
+    names.append(row[1]["Name"])
+# Set initial selection
+name_box.select_set(0)
 
 
 # Buttons
 def save():
-    if messagebox.askyesno("Save", f"Do you want to {r.get()} {name_box.get(name_box.curselection())}?"):
+    name = name_box.get(name_box.curselection())
+    if messagebox.askyesno("Save", f"Do you want to {r.get()} {name}?"):
         # TODO: Create signing in and signing out function, be able to sign in multiple people at once
-        print(f"{r.get()} {name_box.get(name_box.curselection())}")
+        if r.get() == "sign in":
+            signed_in.append(name)
+            now = datetime.now()
+            df.loc[df.index[df["Name"] == name].tolist()[0], current_date] = now.strftime("%H:%M:%S")
+            print(f"{name} signed in at {now.strftime('%H:%M:%S')}")
+        elif r.get() == "sign out":
+            signed_in.remove(name)
+            now = datetime.now()
+            hours_to_add = time_difference(df.loc[df.index[df["Name"] == name].tolist()[0], current_date],
+                                           now.strftime("%H:%M:%S"))
+            df.loc[df.index[df["Name"] == name].tolist()[0], current_date] += f" - {now.strftime('%H:%M:%S')}"
+            df.loc[df.index[df["Name"] == name].tolist()[0], "Hours"] = seconds_to_time(
+            time_to_seconds(df.loc[df.index[df["Name"] == name].tolist()[0], "Hours"]) + hours_to_add)
+            print(f"{name} signed out at {now.strftime('%H:%M:%S')}")
+        else:
+            print("Error: invalid input")
+
 
 
 radioFrame = LabelFrame(root)
 
 r = StringVar()
 r.set("sign in")
+
+search = StringVar()
+search.set("")
 
 sign_in_radio = Radiobutton(radioFrame, text="Sign-in", variable=r, value="sign in", background="light green")
 sign_out_radio = Radiobutton(radioFrame, text="Sign-out", variable=r, value="sign out", background="tomato")
@@ -69,7 +139,8 @@ save_button = Button(root, text="Save", command=save)
 # Pack stuff
 myLabel.pack()
 name_scrollbar.pack(side=RIGHT, fill=Y)
-name_box.pack()
+name_box.pack(pady=10, padx=10, side=LEFT, fill=BOTH, expand=True)
+name_box.focus_set()
 name_frame.pack()
 sign_in_radio.grid(row=0, column=0)
 style = Style(root)
@@ -77,6 +148,42 @@ sign_out_radio.grid(row=0, column=1)
 radioFrame.pack()
 save_button.pack()
 
-root.mainloop()
+# Key bindings
+root.bind("<Return>", lambda event: save())
+root.bind("<Left>", lambda event: r.set("sign in"))
+root.bind("<Right>", lambda event: r.set("sign out"))
+root.bind("<Up>", lambda event: name_box.curselection()[0] - 1 if name_box.curselection()[0] > 0 else 0)
+root.bind("<Down>", lambda event: name_box.curselection()[0] + 1 if name_box.curselection()[0] < len(names) - 1 else len(names) - 1)
+root.bind("<Escape>", lambda event: on_closing(df))
 
-f.close()
+def listbox_search(event):
+    search.set(search.get() + event.char)
+    search_string = search.get()
+    found = False
+    for i in range(len(names)):
+        if names[i].lower().startswith(search_string):
+            name_box.select_clear(0, END)
+            name_box.select_set(i)
+            event.widget.see(i)
+            name_box.event_generate("<<ListboxSelect>>")
+            name_box.see(i)
+            print(i)
+            print("beginning" + search_string)
+            found = True
+            break
+    if not found:
+        for i in range(len(names)):
+            if search_string in names[i].lower():
+                name_box.select_clear(0, END)
+                name_box.selection_set(i)
+                name_box.see(i)
+                print(i)
+                print(search_string)
+                found = True
+                break
+    if not found:
+        search.set("")
+        print("search cleared")
+root.bind("<Key>", lambda event: listbox_search(event))
+
+root.mainloop()
--- testfileoperations.py
+++ testfileoperations.py
@@ -1,15 +1,54 @@
 import pandas as pd
-from datetime import date
-from datetime import time
+from datetime import datetime
+import time
 
 
+def time_difference(start_time, end_time):
+    og = start_time.split(":")
+    new = end_time.split(":")
+    return (int((new[0])) - int(og[0])) * 3600 + (int(new[1]) - int(og[1])) * 60 + (int(new[2]) - int(og[2]))
 
-f = pd.read_csv("RoboticsHourLog.csv")
+def seconds_to_time(seconds):
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = (seconds % 3600) % 60
+    return str(hours) + ":" + str(minutes) + ":" + str(seconds)
+
+def time_to_seconds(time):
+    hours = int(time.split(":")[0])
+    minutes = int(time.split(":")[1])
+    seconds = int(time.split(":")[2])
+    return hours * 3600 + minutes * 60 + seconds
+
+
+df = pd.read_csv("RoboticsHourLog.csv")
+df.to_csv("RoboticsHourLog.csv", index=False)
 
 # print the time
-print(current_time)
+now = datetime.now()
+current_date = now.strftime("%Y-%m-%d")
 
+if not current_date in df.columns:
+    df[current_date] = ""
+
+# Try signing in
+df.loc[df.index[df["Name"] == "Uddish Sood"].tolist()[0], current_date] = now.strftime("%H:%M:%S")
+
+time.sleep(5)
+
+# Try signing out
+now = datetime.now()
+hours_to_add = time_difference(df.loc[df.index[df["Name"] == "Uddish Sood"].tolist()[0], current_date], now.strftime("%H:%M:%S"))
+df.loc[df.index[df["Name"] == "Uddish Sood"].tolist()[0], current_date] += f" - {now.strftime('%H:%M:%S')}"
+df.loc[df.index[df["Name"] == "Uddish Sood"].tolist()[0], "Hours"] = seconds_to_time(time_to_seconds(df.loc[df.index[df["Name"] == "Uddish Sood"].tolist()[0], "Hours"]) + hours_to_add)
+
+
+# CLosing operations
+# Order the rows by the number of hours and update members file
+df = df.sort_values(by=["Hours"], ascending=False)
+# for row in df.iterrows():
+#     f.write(row[1]["Name"] + "\n")
 # writing into the file
-f.to_csv("RoboticsHourLog.csv", index=False)
+df.to_csv("RoboticsHourLog.csv", index=False)
 
-print(f)
+print(df)
